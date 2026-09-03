2eec9d9752479d71deeb97765b7600cc6483dfcb
Zabuzard <zabuza.dev@gmail.com>
2026-09-03 08:00:22 +0200

    fixed Salz not getting categorized right

--- src/de/zabuza/beedlebot/store/ItemDictionary.java
+++ src/de/zabuza/beedlebot/store/ItemDictionary.java
@@ -200,6 +200,7 @@ public final class ItemDictionary {
 		this.mItemNamePatterns.put(".*schreckliches NPC: .*", "schreckliches NPC: Beispiel-NPC");
 		this.mItemNamePatterns.put(".*Duft der .+-Wesen.*", "Duft der XY-Wesen");
 		this.mItemNamePatterns.put(".*Portalkristall von .*", "Portalkristall von Beispieluser");
+		this.mItemNamePatterns.put(".*Salz der .*-Mutation.*", "Salz der XY-Mutation");
 
 		// Dummy item used for items that are inexistent
 		this.mItemNamePatterns.put(".*Geist von .*", "rostiger Werkzeugkoffer");
